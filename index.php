c7ee5d959254b4b8e8d37e85f95f749f1e77477a
LaziukAlena <alena.laziuk1988@gmail.com>
2026-08-27 18:08:11 +0300

    fix: index.php

--- index.php
+++ index.php
@@ -138,8 +138,9 @@
         <div class="header-content">
           <div class="separator"></div>
           <h1 class="header-title">
-            Комплексное обеспечение товарами и&nbsp;расходными материалами
-            бизнеса
+            <?php echo "Комплексное обеспечение товарами и&nbsp;расходными материалами
+            бизнеса"; ?>
+            
           </h1>
           <p class="header-text">
             Высокий уровень вовлечения представителей целевой аудитории является
@@ -724,7 +725,7 @@
         <div class="footer-wrapper">
           <div class="footer-legal">
             <p class="footer-copyright">
-              &copy; 2022 «Aliance Production». Все права защищены.
+              &copy; <?php echo date('Y')?> «Aliance Production». Все права защищены.
             </p>
             <a href="#" class="footer-policy">Политики конфиденциальности</a>
           </div>
